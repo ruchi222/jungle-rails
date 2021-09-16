117bfb75302cb8a534284b1dd5ce6398d72ce7bc
Ruchika Gosain <sharma.ruchika34@gmail.com>
2021-09-16 17:58:12 -0400

    add the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+ 
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
